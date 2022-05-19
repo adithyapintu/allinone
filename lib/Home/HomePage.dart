a79a9adeb8af829b1396624d9bb246967518dd8c
Pintu <bairiadithya113@gmail.com>
2022-05-20 00:40:55 +0530

    Initial Commit

--- lib/Home/HomePage.dart
+++ lib/Home/HomePage.dart
@@ -6,9 +6,45 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body : Container(
-        child: Text("Hello World"),
+      appBar: AppBar(
+        title: const Text("All In One"),
       ),
+      body : Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: const <Widget>[
+          Text("Welcome To AllInOne",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              height: 4.0,
+              color: Colors.indigo,
+              fontSize: 25.0,
+              fontWeight: FontWeight.w800,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          SizedBox(height: 4.0,),
+          Text("A place where you can find all the resources",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 17.0,
+            ),
+          ),
+          SizedBox(height: 30.0,),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 25.0),
+            child: TextField(
+              obscureText: true,
+              cursorWidth: 1.0,
+              cursorHeight: 25.0,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Enter the subject name',
+              ),
+            ),
+          )
+        ],
+      )
     );
   }
 }
