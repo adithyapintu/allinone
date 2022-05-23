2b1f2ba1ce994c8072d108cf4ca2f18ad0bfe635
Pintu <bairiadithya113@gmail.com>
2022-05-23 22:22:31 +0530

    Initial Commit

--- lib/Home/HomePage.dart
+++ lib/Home/HomePage.dart
@@ -19,8 +19,58 @@ class _HomePageState extends State<HomePage> {
     }
 
     return Scaffold(
+        resizeToAvoidBottomInset: false,
         appBar: AppBar(
           title: const Text("All In One"),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            children: <Widget>[
+              UserAccountsDrawerHeader(
+                accountName: const Text("Flutter"),
+                accountEmail: const Text('example@gmail.com'),
+                currentAccountPicture: CircleAvatar(
+                  child: ClipOval(
+                    child: Image.network(
+                      // "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyDgkPQavzX7KwcLzeAsf0fgOx_-D51F3fag&usqp=CAU",
+                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIhsbN3jFVs_7pPgNeGVJ1fqNnS4SN-5XlFw&usqp=CAU",
+                      fit: BoxFit.fill,
+                      height: 90.0,
+                      width: 90.0,
+                    ),
+                  ),
+                ),
+                decoration: const BoxDecoration(
+                  color: Colors.blue,
+                  image: DecorationImage(
+                      fit: BoxFit.fill,
+                      // image: NetworkImage('https://oflutter.com/wp-content/uploads/2021/02/profile-bg3.jpg')
+                      image: NetworkImage("https://c4.wallpaperflare.com/wallpaper/521/942/405/5bd06052e63a0-wallpaper-preview.jpg")
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: const Icon(Icons.favorite),
+                title: const Text('Favorites'),
+                onTap: () => null,
+              ),
+              ListTile(
+                leading: const Icon(Icons.favorite),
+                title: const Text('Favorites'),
+                onTap: () => null,
+              ),
+              ListTile(
+                leading: const Icon(Icons.favorite),
+                title: const Text('Favorites'),
+                onTap: () => null,
+              ),
+              ListTile(
+                leading: const Icon(Icons.favorite),
+                title: const Text('Favorites'),
+                onTap: () => null,
+              ),
+            ],
+          ),
         ),
         body : Container(
           constraints: const BoxConstraints.expand(),
